8b2f3ba6efe748795af5a043365f1912501b9430
rkaundinya <34527220+rkaundinya@users.noreply.github.com>
2025-08-11 16:52:25 +0000

    Deploying to gh-pages from @ rkaundinya/rkaundinya.github.io@40d47de588855b9b49d71186ba4359dbc596c9f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,21 +32,21 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/repositories/";
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "For quick scrolling, you can navigate via the side bar menu. You can also downlaod a pdf of my CV above.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/cv/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/al-folio/teaching/";
